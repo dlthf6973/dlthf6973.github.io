790cdde9797ee1780a989a2d0662fb1655848d0c
Yesol <106740165+dlthf6973@users.noreply.github.com>
2022-06-07 21:52:54 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -18,6 +18,36 @@ $(function () {
         dots: true,
     });
 
+    $('#bgndVideo').YTPlayer();
+
+    $('.utb_movie .left').on('click', function () {
+        $('#bgndVideo').YTPPause();
+    });
+
+    $('.utb_movie .right').on('click', function () {
+        $('#bgndVideo').YTPPlay();
+    });
+
+    $('.customer_tab .tab_menu>li').on('click', function () {
+        var idx = $(this).index(); //0, 1, 2
+        $(this).addClass('on').siblings().removeClass('on');
+        $('.customer_tab .tab_content>div')
+            .eq(idx).addClass('on')
+            .siblings()
+            .removeClass('on');
+
+
+    })
+
+    $('#flink').on('change', function () {
+        var lik = $(this).val();
+        if (lik) window.open(lik);
+    })
+
+    $('.popup button').on('click', function () {
+        $(this).parent().hide();
+    })
+
 })
 
 
